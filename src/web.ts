2da405a0333a27e1d647b2753f96b8738aa2d381
caelondev <jerichopanagsagan0@gmail.com>
2025-11-09 13:28:06 +0800

    Set "trust proxy" to true in web.ts

--- src/web.ts
+++ src/web.ts
@@ -4,6 +4,8 @@ import { PrettyLogger as log, LogTag } from "./utils/PrettyLogger"
 const app = express()
 const PORT = process.env.PORT || 8080
 
+app.set("trust proxy", true)
+
 app.get("/", (req, res)=>{
   res.send("Bot is online")
 })
